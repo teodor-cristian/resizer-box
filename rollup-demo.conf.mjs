7ebfcdd8826efa8c3861a725ac1882f2b3c88952
Teodor Solca <teodor.cristian.s@gmail.com>
2024-09-23 18:08:36 +0300

    Added google analytics to the demo website
    Added google analytics to the demo website

--- rollup-demo.conf.mjs
+++ rollup-demo.conf.mjs
@@ -1,13 +1,19 @@
 import { rollupPluginHTML as html } from '@web/rollup-plugin-html';
 import terser from "@rollup/plugin-terser";
 
+const GOOGLE_ANALYTICS_CODE = "G-P775S8ZZXZ";
+
 export default {
   input: 'demo/index.html',
   output: {
     dir: 'demo-build'
   },
   plugins: [
-    html(),
-    terser()
+    html({
+      transformHtml: [
+        html => html.replaceAll('GOOGLE_ANALYTICS_CODE', GOOGLE_ANALYTICS_CODE)
+      ],
+    }),
+    terser(),
   ],
 };
